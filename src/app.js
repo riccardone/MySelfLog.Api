69b9aa7683bfa3fe6bab077b038e1ca61cc5f76a
riccardone <riccardo@dinuzzo.it>
2018-01-14 17:28:49 +0000

    removed securitylink; passing through new commands

--- src/app.js
+++ src/app.js
@@ -38,38 +38,68 @@ app.get('/', function (req, res) {
     res.status(200).send('ok');
 });
 
-app.get('/api/v1/diary/securitylink/:profile', function (req, res) {
-    var id = eventDataMapper.getCorrelationId(req.params.profile);
-    // Query elastic and get the securityLink    
-    _client.get({
+// app.get('/api/v1/diary/securitylink/:profile', function (req, res) {
+//     var id = eventDataMapper.getCorrelationId(req.params.profile);
+//     // Query elastic and get the securityLink    
+//     _client.get({
+//         index: "diary-events",
+//         type: "diaryEvent",
+//         id: id
+//     }).then(d => {
+//         res.send(d.text());
+//     }).catch(err => {
+//         if (!err.statusCode) {
+//             res.status(500).send('error');
+//         } else {
+//             res.status(err.statusCode).send('error');
+//         }
+//         logger.error(err);
+//     });
+// });
+
+app.get('/api/v1/diary/check/:diaryName', function (req, res) {
+    // Query elastic and check if this diaryName is already in use   
+    var diaryName = req.params.diaryName;
+    _client.search({
         index: "diary-events",
-        type: "diaryEvent",
-        id: id
+        q: 'DiaryName:"' + diaryName + '"'
     }).then(d => {
-        res.send(d);
+        if (d.hits.total > 0) {
+            return res.status(201).send("not available");
+        }
+        return res.status(201).send("available");
     }).catch(err => {
-        if (err.statusCode === 404) {
-            res.status(404).send('Not found');
+        if (!err.statusCode) {
+            res.status(500).send('error');
+        } else {
+            res.status(err.statusCode).send('error');
         }
         logger.error(err);
     });
 });
 
-app.post('/api/v1/diary', function (req, res) {
-    // TODO validate request
-    // if (validation.requestNotValid(req)) {
-    //     var errorMessage = "Request body not valid";
-    //     logger.error(errorMessage);
-    //     return res.status(400).send(errorMessage);
-    // }       
-    sender.send(req.body, 'DiaryEventReceived').then(function (result) {
-        logger.debug("Event stored");
-        res.status(201).send(); // TODO redirect on the diary?
-    }).catch(function (err) {
+app.get('/api/v1/diary/:profile', function (req, res) {
+    var id = eventDataMapper.getCorrelationId(req.params.profile);
+    // Query elastic and check if this diaryName is already in use   
+    var diaryName = req.params.diaryName;
+    _client.get({
+        index: "diary-events",
+        type: "diaryEvent",
+        id: id
+    }).then(d => {
+        if (d.found) {            
+            return res.status(201).send(d._source);    
+        }
+        return res.status(201).send("not available");
+    }).catch(err => {
+        if (!err.statusCode) {
+            res.status(500).send('error');
+        } else {
+            res.status(err.statusCode).send('error');
+        }
         logger.error(err);
-        res.status(500).send('There is a technical problem and the log has not been stored');
     });
-})
+});
 
 app.post('/api/v1/logs', function (req, res) {
     if (validation.requestNotValid(req)) {
@@ -92,7 +122,8 @@ app.post('/api/v1/diary', function (req, res) {
         logger.error(errorMessage);
         return res.status(400).send(errorMessage);
     }
-    sender.send(req.body, 'DiaryEventReceived').then(function (result) {
+    // TODO change this nonsense event type and use a proper one
+    sender.send(req.body, 'CreateDiary').then(function (result) {
         logger.debug("Event stored");
         res.status(201).send(); // TODO redirect on the diary?
     }).catch(function (err) {
