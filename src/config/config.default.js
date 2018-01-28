75de4884426f68e39e28062ec0367894911da1f5
riccardone <riccardo@dinuzzo.it>
2018-01-28 19:57:54 +0000

    fix config

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -6,8 +6,8 @@ config.port = process.env.App_Port;
 
 config.projectName = "MySelfLog-Api"; // no spaces or strange chars please
 
-config.eventstoreConnection = process.env.Eventstore_Connection;
-config.eventstoreConnectionSettings = process.env.Eventstore_Connection_Settings;
+config.eventstoreConnection = process.env.Eventstore_Connection || 'tcp://localhost:1113';
+config.eventstoreConnectionSettings = process.env.Eventstore_Connection_Settings || {};
 config.publishTo = "diary-input";
 
 config.elasticSearchLink = process.env.Elastic_Host;
