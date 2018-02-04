17c5b92c0854e3ee7b59817851558c95f9a94adb
riccardone <riccardo@dinuzzo.it>
2018-02-04 12:32:21 +0000

    minor config change

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -8,7 +8,7 @@ config.projectName = "myselflog-api";
 
 config.eventstoreConnection = process.env.EventStore_Link || 'tcp://eventstore:1113';
 config.eventstoreConnectionSettings = process.env.Eventstore_Connection_Settings || {'admin':'changeit'};
-config.publishTo = "diary-input";
+config.publishTo = process.env.Eventstore_Publish_To || "diary-input";
 
 config.elasticSearchLink = process.env.Elastic_Link || 'http://elasticsearch:9200';
 
