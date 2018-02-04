ed55b114caf78594a229da4c0ed1146b6990beb1
riccardone <riccardo@dinuzzo.it>
2018-02-04 11:44:27 +0000

    config change

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -7,7 +7,7 @@ config.port = process.env.App_Port;
 config.projectName = "myselflog-api"; // no spaces or strange chars please
 
 config.eventstoreConnection = process.env.EventStore_Link || 'tcp://eventstore:1113';
-config.eventstoreConnectionSettings = process.env.Eventstore_Connection_Settings || {admin:changeit};
+config.eventstoreConnectionSettings = process.env.Eventstore_Connection_Settings || {'admin':'changeit'};
 config.publishTo = "diary-input";
 
 config.elasticSearchLink = process.env.Elastic_Link;
