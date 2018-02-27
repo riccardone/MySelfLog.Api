0161a17827fe623c659c5fced21e8783641c16e8
riccardone <riccardo@dinuzzo.it>
2018-02-27 22:33:20 +0000

    more unit tests

---
+++ src/service.js
@@ -0,0 +1,122 @@
+const express = require('express')
+// var swaggerUi = require('swagger-ui-express');
+// var swaggerDocument = require('./swagger.json');
+// https://blog.cloudboost.io/adding-swagger-to-existing-node-js-project-92a6624b855b
+var bodyParser = require('body-parser');
+var validation = require('./validation');
+var eventDataMapper = require('./eventdata-mapper');
+
+const app = express();
+app.use(bodyParser.json());
+
+var _sender;
+var _publishTo = "diary-input";
+var _esClient;
+var _logger;
+
+function Service(sender, esClient, logger) {   
+    _sender = sender;
+    _esClient = esClient;
+    _logger = logger;
+}
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('ok');
+});
+
+app.get('/api/v1/diary/check/:diaryName', function (req, res) {
+    // Query elastic and check if this diaryName is already in use   
+    var diaryName = req.params.diaryName;
+    _esClient.search({
+        index: "diary-events",
+        q: 'DiaryName:"' + diaryName + '"'
+    }).then(d => {
+        if (d.hits.total > 0) {
+            return res.status(201).send("not available");
+        }
+        return res.status(201).send("available");
+    }).catch(err => {
+        if (err.message.startsWith("[index_not_found_exception]")) {
+            return res.status(201).send("available");
+        }
+        if (!err.statusCode) {
+            res.status(500).send('error');
+        } else {
+            res.status(err.statusCode).send('error');
+        }
+        _logger.error(err);
+    });
+});
+
+app.get('/api/v1/diary/:profile', function (req, res) {
+    var id = eventDataMapper.getCorrelationId(req.params.profile);
+    // Query elastic and check if this diaryName is already in use   
+    //var diaryName = req.params.diaryName;
+    _esClient.get({
+        index: "diary-events",
+        type: "diaryEvent",
+        id: id
+    }).then(d => {
+        if (d.found) {
+            return res.status(201).send(d._source);
+        }
+        return res.status(201).send("not available");
+    }).catch(err => {
+        if (err.message.startsWith("[index_not_found_exception]")) {
+            return res.status(err.statusCode).send("index not found");
+        }
+        if (!err.statusCode) {
+            res.status(500).send('error');
+        } else {
+            res.status(err.statusCode).send('error');
+        }
+        _logger.error(err);
+    });
+});
+
+app.post('/api/v1/logs', function (req, res) {
+    if (validation.requestNotValid(req)) {
+        var errorMessage = "Request body not valid";
+        _logger.error(errorMessage);
+        return res.status(400).send(errorMessage);
+    }
+    sender.send(req.body, 'LogReceived').then(function (result) {
+        _logger.debug("Event stored");
+        res.send(); // TODO redirect on the diary?
+    }).catch(function (err) {
+        _logger.error(err);
+        res.status(500).send('There is a technical problem and the log has not been stored');
+    });
+})
+
+app.post('/api/v1/diary', function (req, res) {
+    if (validation.requestNotValid(req)) {
+        var errorMessage = "Request body not valid";
+        _logger.error(errorMessage);
+        return res.status(400).send(errorMessage);
+    }
+    // TODO change this nonsense event type and use a proper one
+    sender.send(req.body, 'CreateDiary').then(function (result) {
+        _logger.debug("Event stored");
+        res.send(); // TODO redirect on the diary?
+    }).catch(function (err) {
+        _logger.error(err);
+        res.status(500).send('There is a technical problem and the log has not been stored');
+    });
+})
+
+var server = app.listen(5001, '0.0.0.0', function () {
+    var address = server.address().address + ":" + server.address().port;
+    
+    _logger.info("MySelfLog-Api listening on %s", address)
+    _logger.info("MySelfLog-Api publishing to %s", _publishTo)
+});
+
+Service.prototype.server = server;
+module.exports = Service;
--- test/app.test.js
+++ test/app.test.js
@@ -1,19 +1,51 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../src/app');
+var sinon = require('sinon')
+//let server = require('../src/app');
+let serviceModule = require('../src/service');
+let senderModule = require('../src/messageSender');
 let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('/', () => {        
+var sender = new senderModule(null, "test");
+const fakeEsClient = sinon.stub();
+fakeEsClient.prototype.get = sinon.stub().resolves({ found: true, _source: "test" });
+var esClient = new fakeEsClient();
+const fakeLogger = sinon.stub();
+fakeLogger.prototype.error = sinon.stub();
+fakeLogger.prototype.info = sinon.stub();
+var service = new serviceModule(sender, esClient, new fakeLogger());
+
+describe('/', () => {
   describe('/GET data', () => {
-      it('it should GET 200 (healty check)', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);               
-              done();
-            });
-      });
+    it('it should GET 200 (healty check)', (done) => {
+      chai.request(service.server)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it('it should GET ciccio', (done) => {
+      chai.request(service.server)
+        .get('/api/v1/diary/ciccio')
+        .end((err, res) => {
+          res.should.have.status(201);
+          done();
+        });
+    });
+  });
+});
+describe('/POST /api/v1/diary', () => {
+  describe('/POST wrong data', () => {
+    it('it should POST 400 (healty check)', (done) => {
+      chai.request(service.server)
+        .post('/api/v1/diary')
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
   });
 });
