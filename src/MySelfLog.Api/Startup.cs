08b72a22f1f9b0206045e499d1f2f50d0376780d
Riccardo Di Nuzzo <riccardo.dinuzzo@chubb.com>
2020-12-26 17:44:49 +0000

    ...

--- src/MySelfLog.Api/Startup.cs
+++ src/MySelfLog.Api/Startup.cs
@@ -121,6 +121,7 @@ namespace MySelfLog.Api
                     "Open",
                     builder => builder.AllowAnyOrigin().AllowAnyHeader());
             });
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
