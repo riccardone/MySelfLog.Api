4e8a440aa37eb4826e3162ddbb043b3536fb2256
Riccardo Di Nuzzo <riccardo.dinuzzo@chubb.com>
2020-12-26 23:02:40 +0000

    App Service deploy

--- src/MySelfLog.Api/Program.cs
+++ src/MySelfLog.Api/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MySelfLogApi
+namespace MySelfLog.Api
 {
     public class Program
     {
--- src/MySelfLog.Api/Startup.cs
+++ src/MySelfLog.Api/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MySelfLogApi
+namespace MySelfLog.Api
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace MySelfLogApi
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MySelfLogApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MySelfLog.Api", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace MySelfLogApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MySelfLogApi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MySelfLog.Api v1"));
             }
 
             app.UseHttpsRedirection();
